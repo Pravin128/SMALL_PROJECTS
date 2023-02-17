4a854d0ae57332dd3eabaedecb32769a3c20023d
CODE_WITH_PRAVIN <121967042+Pravin128@users.noreply.github.com>
2023-02-17 10:21:05 +0530

    Create dice_game.py

---
+++ dice_game.py
@@ -0,0 +1,22 @@
+print("""this is the dice game.......
+lets see in how many attempts tou can catch the number!!!""")
+import random
+a=1
+rand=0
+num=0
+count=0
+while a!=0:
+    num=int(input("enter number: "))
+    for i in range(7):
+        rand=random.randint(0,6)
+    print(rand)
+    if num>6:
+        print("please enter number less than 7")
+    elif num==rand:
+        print("you got it!!!")
+        break
+    else:
+        count+=1
+    a+=1
+print(f"You Got It In {count} chances")
+
